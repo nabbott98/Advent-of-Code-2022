d58e3534ec3d0e757ab1e9dcbd863aa26f048bec
nabbott98 <nicholasabbott98@gmail.com>
2022-12-15 13:07:08 -0600

    Challenge 1 Complete

--- Day-1-Challenge-1/calories.py
+++ Day-1-Challenge-1/calories.py
@@ -1,8 +1,26 @@
+# Open input document
 elves = open("input.txt", "r")
 
+# Read input document and save to variable
 data = elves.read()
+elves.close()
 
+# Split list by newline
 elves_list = data.split("\n")
 
-print(elves_list)
-elves.close()
+# Declare new array for elf totals and variable to tally up each elfs total
+elf_totals = []
+total = 0
+
+# Iterate through list once and add each consecutive number until a blank string
+for i in elves_list:
+    # is this indicie a blank string?
+    if i == '':
+        elf_totals.append(total)
+        total = 0
+    # if not add to total
+    else:
+        total += int(i)
+
+# print max value in new list! Yummy!
+print('The elf with the most calories is carrying', max(elf_totals), 'calories! Yummy!')
